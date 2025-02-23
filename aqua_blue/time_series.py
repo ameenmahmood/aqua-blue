c71ae65f74ddd54a72519c8f29b18fe0fd7f4265
Ameen Mahmood <ameenbmahmood@gmail.com>
2025-02-23 17:27:06 -0500

    datetime change v2

--- aqua_blue/time_series.py
+++ aqua_blue/time_series.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from dataclasses import dataclass
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 
 from numpy.typing import NDArray
@@ -16,8 +16,11 @@ class TimeSeries:
     times: NDArray
 
     def __post_init__(self):
+        # Convert integer-based time indices to datetime starting from a fixed date
+        start_date = datetime(1970, 1, 1)
+        self.times = np.array([start_date + timedelta(days=t) for t in self.times])
         
-        timesteps = np.diff(self.times)
+        timesteps = np.diff(self.times.astype('datetime64[D]').astype(float))
 
         if not np.isclose(np.std(timesteps), 0.0):
             raise ValueError("TimeSeries.times must be uniformly spaced")
@@ -51,7 +54,7 @@ class TimeSeries:
     @property
     def timestep(self) -> float:
 
-        return self.times[1] - self.times[0]
+        return (self.times[1] - self.times[0]).days # returns timestep in days
 
     def __eq__(self, other) -> bool:
 
