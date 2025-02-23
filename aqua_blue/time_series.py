1de00badd803b5c501cc6985f7dac86b45d9a7cc
Ameen Mahmood <ameenbmahmood@gmail.com>
2025-02-23 16:54:59 -0500

    revert back before v1 change

--- aqua_blue/time_series.py
+++ aqua_blue/time_series.py
@@ -17,8 +17,6 @@ class TimeSeries:
 
     def __post_init__(self):
         
-        if isinstance(self.times[0], datetime):
-            self.times = np.array([t.timestamp() for t in self.times], dtype=np.float64)   
         timesteps = np.diff(self.times)
 
         if not np.isclose(np.std(timesteps), 0.0):
@@ -44,28 +42,16 @@ class TimeSeries:
     @classmethod
     def from_csv(cls, fp: Union[IO, str, Path], time_index: int = 0):
 
-        data = pd.read_csv(fp)
-        # Check if time column contains datetime values
-        if isinstance(data.iloc[0, time_index], str):  
-            data.iloc[:, time_index] = pd.to_datetime(data.iloc[:, time_index])
-            times = data.iloc[:, time_index].apply(lambda x: x.timestamp()).values  # Convert to UNIX timestamps
-        else:
-            times = data.iloc[:, time_index].values  # Already numerical
+        data = np.loadtxt(fp, delimter=",")
         return cls(
-            dependent_variable=data.delete(data.columns[time_index], axis=1).values, 
-            times=data[:, time_index]
+            dependent_variable=np.delete(data, obj=time_index, axis=1),
+            times=data[:,time_index]
         )
 
     @property
     def timestep(self) -> float:
 
         return self.times[1] - self.times[0]
-    
-    def to_datetime(self):
-        """
-        Convert stored UNIX timestamps back to datetime objects.
-        """
-        return [datetime.utcfromtimestamp(t) for t in self.times]
 
     def __eq__(self, other) -> bool:
 
